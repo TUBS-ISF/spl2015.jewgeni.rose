049a9218e7e16891db501f02ebcef0dbbfd94b7a
bakaranger <j.rose@tu-bs.de>
2015-05-25 15:05:10 +0200

    - import and export functions done - model changed to fit new implementation

--- jOrganizer/src/main/java/de/tubs/cs/isf/spl/jorg/calendar/exports/CsvExporter.java
+++ jOrganizer/src/main/java/de/tubs/cs/isf/spl/jorg/calendar/exports/CsvExporter.java
@@ -2,6 +2,8 @@ package de.tubs.cs.isf.spl.jorg.calendar.exports;
 
 import de.tubs.cs.isf.spl.jorg.calendar.Meeting;
 
+import java.time.format.DateTimeFormatter;
+
 import static de.tubs.cs.isf.spl.jorg.App.app;
 
 /**
@@ -16,11 +18,13 @@ public class CsvExporter extends Exporter {
     @Override
     public String format() {
         final StringBuilder sb = new StringBuilder();
-        sb.append("Meetings for '").append(app().currentUser()).append("'\n");
-        sb.append("-------------------------------\n\n");
+        sb.append("Title;Place;Date;Duration;Note;").append("\n");
         for (final Meeting m : app().calendar().meetings()) {
-            sb.append(m.toString());
-            sb.append("-------------------------------\n");
+            sb.append("\"").append(m.title())
+                    .append("\"; \"").append(m.place())
+                    .append("\"; ").append(m.date().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+                    .append("; ").append(m.duration().toMinutes()).append("min")
+                    .append("; \"").append(m.note()).append("\";\n");
         }
         return sb.toString();
     }
--- jOrganizer/src/main/java/de/tubs/cs/isf/spl/jorg/calendar/exports/IcsExporter.java
+++ jOrganizer/src/main/java/de/tubs/cs/isf/spl/jorg/calendar/exports/IcsExporter.java
@@ -2,6 +2,9 @@ package de.tubs.cs.isf.spl.jorg.calendar.exports;
 
 import de.tubs.cs.isf.spl.jorg.calendar.Meeting;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 import static de.tubs.cs.isf.spl.jorg.App.app;
 
 /**
@@ -9,19 +12,41 @@ import static de.tubs.cs.isf.spl.jorg.App.app;
  */
 public class IcsExporter extends Exporter {
 
+    private static final String FOOTER = "END:VCALENDAR\n";
+    private final DateFormat dateFormat;
+
     public IcsExporter(final String name) {
         super(name);
+        dateFormat = new SimpleDateFormat("yyyyMMdd'T'HHmmss");
     }
 
     @Override
     public String format() {
         final StringBuilder sb = new StringBuilder();
-        sb.append("Meetings for '").append(app().currentUser()).append("'\n");
-        sb.append("-------------------------------\n\n");
+        sb.append(generateHeader());
         for (final Meeting m : app().calendar().meetings()) {
-            sb.append(m.toString());
-            sb.append("-------------------------------\n");
+            sb.append("BEGIN:VEVENT").append("\n");
+            sb.append("UID:").append(m.hashCode()).append("@example.com").append("\n");
+            sb.append("SUMMARY:").append(m.title()).append("\n");
+            sb.append("LOCATION:").append(m.place()).append("\n");
+            sb.append("DESCRIPTION:").append(m.note()).append("\n");
+            sb.append("DTSTART:").append(dateFormat.format(m.date())).append("\n");
+            sb.append("DTEND:").append(dateFormat.format(m.date().plus(m.duration()))).append("\n");
+            sb.append("CLASS:PRIVATE").append("\n");
+            sb.append("END:VEVENT").append("\n");
         }
+        sb.append(FOOTER);
+        return sb.toString();
+    }
+
+    private static String generateHeader() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("BEGIN:VCALENDAR").append("\n");
+        sb.append("VERSION:2.0").append("\n");
+        sb.append("PRODID:http://www.example.com/").append(app().currentUser()).append("/calendar/").append("\n");
+        sb.append("METHOD:PUBLISH").append("\n");
+        sb.append("CALSCALE:GREGORIAN").append("\n");
+        sb.append("BEGIN:VCALENDAR").append("\n");
         return sb.toString();
     }
 }
